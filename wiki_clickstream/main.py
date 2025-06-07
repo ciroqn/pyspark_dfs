8f77a6e2edf2b941efa9d11926b005408b3c6069
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2025-06-07 16:48:15 +0100

    Create main.py

---
+++ wiki_clickstream/main.py
@@ -0,0 +1,26 @@
+# Analysing the traffic to Wiki pages via open source info
+
+from pyspark.sql import SparkSession
+
+# Create a new SparkSession
+spark = SparkSession\
+.builder\
+.config('spark.app.name', 'wiki_clickstreams')\
+.getOrCreate()
+
+# Sample data
+sample_clickstream_counts = [
+    ["other-search", "Hanging_Gardens_of_Babylon", "external", 47000],
+    ["other-empty", "Hanging_Gardens_of_Babylon", "external", 34600],
+    ["Wonders_of_the_World", "Hanging_Gardens_of_Babylon", "link", 14000],
+    ["Babylon", "Hanging_Gardens_of_Babylon", "link", 2500]
+]
+
+# Create RDD from sample data
+clickstream_counts_rdd = spark.sparkContext.parallelize(sample_clickstream_counts)
+
+# Create a DataFrame from the RDD of sample clickstream counts (col names are give in teh list arg.
+clickstream_sample_df = clickstream_counts_rdd.toDF(['source_page', 'target_page', 'link_category', 'link_count'])
+
+# Display the DataFrame to the notebook
+clickstream_sample_df.show(5, truncate=False)
